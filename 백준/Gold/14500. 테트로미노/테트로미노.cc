1fdc6a4b7e33ebf72d06ecef515e30881205a362
katessing <66496527+katessing@users.noreply.github.com>
2026-04-20 23:38:02 +0900

    [Gold IV] Title: 테트로미노, Time: 12 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.cc
+++ 백준/Gold/14500. 테트로미노/테트로미노.cc
@@ -2,104 +2,131 @@
 
 using namespace std;
 
-int map[505][505];
-bool visited[505][505];
+int map[500][500];
 
-int dx[] = { 1, 0, -1, 0 };
-int dy[] = { 0, -1, 0, 1 };
-
-int R, C;
-
-int COST = 0;
-
-
-// (x,y)
-int Shape[][4][2] = {
-	{{0, 0},{1, 0}, {2, 0}, { 1,1 }},
-	{{0, 1},{1, 1}, {2, 1}, { 1,0 }},
-	{{0, 0},{0, 1}, {0, 2}, { 1,1 }},
-	{{1, 0},{1, 1}, {1, 2}, { 0,1 }}
-};
-
-void dfs(int x, int y, int cost, int size)
-{
-	int current_cost = cost + map[y][x];
-
-	if (size == 4)
-	{
-		if (current_cost > COST) COST = current_cost;
-		
-		return;
-	}
-
-	for (int i = 0; i < 4; i++)
-	{
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-
-		if (nx < 0 || nx >= C || ny < 0 || ny >= R) continue;
-
-		if (!visited[ny][nx])
-		{
-			visited[ny][nx] = true;
-			dfs(nx, ny, current_cost, size + 1);
-			visited[ny][nx] = false;
-		}
-	}
-}
+// 2 x 3 - 2
+// 3 x 2 - 2
+// 2 x 2
+// 1 x 4
+// 4 x 1
 
 int main()
 {
-	cin >> R >> C;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
-	for (int i = 0; i < R; i++)
+	int r, c;
+	cin >> r >> c;
+
+	for (int i = 0; i < r; i++)
 	{
-		for (int j = 0; j < C; j++)
+		for (int j = 0; j < c; j++)
 		{
 			cin >> map[i][j];
 		}
 	}
 
-	for (int y = 0; y < R; y++)
+	int sum = 0;
+	for (int i = 0; i < r - 1; i++)
 	{
-		for (int x = 0; x < C; x++)
+		for (int j = 0; j < c - 1; j++)
 		{
-			visited[y][x] = true;
-			dfs(x, y, 0, 1);
-			visited[y][x] = false;
+			int area = map[i][j] + map[i+1][j] + map[i][j+1] + map[i+1][j+1];
+			if (sum < area) sum = area;
+		}
+	}
+	
+	for (int i = 0; i < r - 3; i++)
+	{
+		for (int j = 0; j < c; j++)
+		{
+			int area = map[i][j] + map[i + 1][j] + map[i+2][j] + map[i + 3][j];
+			if (sum < area) sum = area;
+		}
+	}
+	
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < c - 3; j++)
+		{
+			int area = map[i][j] + map[i][j+1] + map[i][j + 2] + map[i][j + 3];
+			if (sum < area) sum = area;
+		}
+	}
+	
+	//[][]
+	//[][]
+	//[][]
+	//3 x 2
+	for (int i = 0; i < r - 2; i++)
+	{
+		for (int j = 0; j < c - 1; j++)
+		{
+			int area = map[i][j] + map[i+1][j] + map[i+2][j]
+				+ map[i][j+1] + map[i + 1][j + 1] + map[i + 2][j + 1];
+			int shape = area - map[i][j] -map[i+1][j];
+			if (sum < shape) sum = shape;
+			shape = area - map[i+1][j] -map[i+2][j];
+			if (sum < shape) sum = shape;
+			shape = area - map[i][j+1] -map[i + 1][j + 1];
+			if (sum < shape) sum = shape;
+			shape = area - map[i + 1][j + 1] -map[i + 2][j + 1];
+			if (sum < shape) sum = shape;
+			shape = area - map[i][j] -map[i+2][j];
+			if (sum < shape) sum = shape;
+			shape = area - map[i][j+1] -map[i+2][j + 1];
+			if (sum < shape) sum = shape;
+			shape = area - map[i][j] -map[i + 2][j + 1];
+			if (sum < shape) sum = shape;
+			shape = area - map[i][j+1] -map[i+2][j];
+			if (sum < shape) sum = shape;
 		}
 	}
 
-
-	for (int y = 0; y < R-1; y++)
+	//[][][]
+	//[][][]
+	//yx
+	//2 x 3
+	for (int i = 0; i < r - 1; i++)
 	{
-		for (int x = 0; x < C-2; x++)
+		for (int j = 0; j < c - 2; j++)
 		{
-			for (int s = 0; s < 2; s++)
-			{
-				int cost = map[Shape[s][0][1] + y][Shape[s][0][0] + x]
-					+ map[Shape[s][1][1] + y][Shape[s][1][0] + x]
-					+ map[Shape[s][2][1] + y][Shape[s][2][0] + x]
-					+ map[Shape[s][3][1] + y][Shape[s][3][0] + x];
-				if (cost > COST) COST = cost;
-			}
+			int area = map[i][j] + map[i][j + 1] + map[i][j + 2]
+				+ map[i + 1][j] + map[i + 1][j + 1] + map[i + 1][j + 2];
+			//[x][x][]
+			//[][][]
+			int shape = area - map[i][j] - map[i][j + 1];
+			if (sum < shape) sum = shape;
+			//[][x][x]
+			//[][][]
+			shape = area - map[i][j+1] -map[i][j + 2];
+			if (sum < shape) sum = shape;
+			//[][][]
+			//[x][x][]
+			shape = area - map[i+1][j] -map[i+1][j + 1];
+			if (sum < shape) sum = shape;
+			//[][][]
+			//[][x][x]
+			shape = area - map[i + 1][j+1] -map[i + 1][j + 2];
+			if (sum < shape) sum = shape;
+			//[x][][x]
+			//[][][]
+			shape = area - map[i][j] -map[i][j + 2];
+			if (sum < shape) sum = shape;
+			//[][][]
+			//[x][][x]
+			shape = area - map[i+1][j] -map[i+1][j + 2];
+			if (sum < shape) sum = shape;
+			//[x][][]
+			//[][][x]
+			shape = area - map[i][j] -map[i + 1][j + 2];
+			if (sum < shape) sum = shape;
+			//[][][x]
+			//[x][][]
+			shape = area - map[i+1][j] -map[i][j + 2];
+			if (sum < shape) sum = shape;
 		}
 	}
 
-	for (int y = 0; y < R - 2; y++)
-	{
-		for (int x = 0; x < C - 1; x++)
-		{
-			for (int s = 2; s < 4; s++)
-			{
-				int cost = map[Shape[s][0][1] + y][Shape[s][0][0] + x]
-					+ map[Shape[s][1][1] + y][Shape[s][1][0] + x]
-					+ map[Shape[s][2][1] + y][Shape[s][2][0] + x]
-					+ map[Shape[s][3][1] + y][Shape[s][3][0] + x];
-				if (cost > COST) COST = cost;
-			}
-		}
-	}
-
-	cout << COST;
+	cout << sum;
 }
