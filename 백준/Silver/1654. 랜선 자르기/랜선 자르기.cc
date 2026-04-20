71dc514b5483006a056546fc957aa2995b7613e9
katessing <66496527+katessing@users.noreply.github.com>
2026-04-20 23:58:07 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,63 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-long long Check(const vector<int>& input, long long x)
-{
-	long long count = 0;
-	for (int i : input)
-	{
-		count += i / x;
-	}
-
-	return count;
-}
-
-//int BinarySearch(function<int(const vector<int>&, int)> fun, int x)
-//{
-//
-//}
-
-int BinarySearch(const vector<int>& input, int x)
-{
-	long long start = 1;
-	long long end = *max_element(input.begin(), input.end());
-	long long ans = 0;
-	while (start <= end)
-	{
-		long long mid = start + (end - start) / 2;
-		long long value = Check(input, mid);
-		if (value >= x)
-		{
-			ans = mid;
-			start = mid + 1;
-		}
-		else
-		{
-			end = mid - 1;
-		}
-		
-	}
-	return ans;
-}
-
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	int k, n;
-
-	cin >> k >> n;
-	vector<int> line(k);
-
-	for (int& l : line)
-	{
-		cin >> l;
-	}
-
-	cout << BinarySearch(line, n);
-
-}
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
